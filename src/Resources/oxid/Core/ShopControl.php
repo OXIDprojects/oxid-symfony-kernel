915934151c52060de193c932f30ea3e807b0049b
Sascha Weidner <=>
2019-05-03 13:56:23 +0200

    Add cl bypass and security controller

--- src/Resources/oxid/Core/ShopControl.php
+++ src/Resources/oxid/Core/ShopControl.php
@@ -4,19 +4,28 @@ namespace Sioweb\Oxid\Kernel\Legacy\Core;
 
 use Sioweb\Oxid\Kernel\HttpKernel\Kernel;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 class ShopControl extends ShopControl_parent
 {
     public function start($controllerKey = null, $function = null, $parameters = null, $viewsChain = null)
     {
+        if(!empty($_GET['cl'])) {
+            parent::start($controllerKey, $function, $parameters, $viewsChain);
+        }
+
+        $_GET = array_merge($_GET, [
+            '_controller' => '-1'
+        ]);
+
+        // Debug::enable(E_ERROR);
+        Request::enableHttpMethodParameterOverride();
         $kernel = new Kernel('prod', false);
-        $kernel->loadClassCache();
-        //$kernel = new AppCache($kernel);
-        
+    
         $request = Request::createFromGlobals();
         $response = $kernel->handle($request);
         $responseStatusCode = $response->getStatusCode();
-        
+
         if($responseStatusCode !== 404) {
             $response->send();
             $kernel->terminate($request, $response);
